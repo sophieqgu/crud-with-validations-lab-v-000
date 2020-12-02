4708dd6f06bbb2f35968f259dc42c0ed697e72f7
Sophie Gu <sophieqgu@gmail.com>
2020-12-02 05:12:27 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+  validates :title, presence: true, uniqueness: { scope: %i[:release_year :artist_name] }
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, unless: -> { released.is_false? }, numericality: { less_than_or_equal_to: Date.today.year }
+  validates :artist_name, presence: true
 end
